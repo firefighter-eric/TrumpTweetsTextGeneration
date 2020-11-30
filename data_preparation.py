e64a640f6459687bc972ff53c176ff717bde8652
Qifan Zhang <firefighter-eric@users.noreply.github.com>
2020-11-29 21:30:32 -0500

    model update

--- data_preparation.py
+++ data_preparation.py
@@ -10,7 +10,6 @@ def text2tf_data(filename, seq_length, batch_size, buffer_size):
     idx2char = np.array(vocab)
     text_as_int = np.array([char2idx[c] for c in text])
 
-    # seq_length = 100
     examples_per_epoch = len(text) // (seq_length + 1)
     char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
     sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
--- text_generation.py
+++ text_generation.py
@@ -3,11 +3,10 @@ import os
 import numpy as np
 
 from data_preparation import *
+from model import *
 
 BATCH_SIZE = 64
 BUFFER_SIZE = 10000
-
-
 dataset, char2idx, idx2char, vocab = text2tf_data(filename='donald_tweets.txt',
                                                   seq_length=100,
                                                   buffer_size=BUFFER_SIZE,
@@ -17,39 +16,19 @@ VOCAB_SIZE = len(vocab)
 EMBEDDING_DIM = 256
 RNN_UNIT = 1024
 
-
-
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                                  batch_input_shape=[batch_size, None]),
-        tf.keras.layers.GRU(rnn_units,
-                            return_sequences=True,
-                            stateful=True,
-                            recurrent_initializer='glorot_uniform'),
-        tf.keras.layers.Dense(vocab_size)
-    ])
-    return model
-
-
 model = build_model(
     vocab_size=VOCAB_SIZE,
     embedding_dim=EMBEDDING_DIM,
     rnn_units=RNN_UNIT,
     batch_size=BATCH_SIZE)
-
 model.summary()
 
-
-def loss(labels, logits):
-    return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
-
-
-model.compile(optimizer='adam', loss=loss)
+LOSS = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+OPTIMIZER = 'adam'
+model.compile(optimizer=OPTIMIZER, loss=LOSS)
 
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-
 checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_prefix,
     save_weights_only=True)
@@ -63,9 +42,7 @@ model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 model.build(tf.TensorShape([1, None]))
 
 
-def generate_text(model, start_string):
-    num_generate = 1000
-
+def generate_text(model, start_string, num_generate):
     input_eval = [char2idx[s] for s in start_string]
     input_eval = tf.expand_dims(input_eval, 0)
 
@@ -85,10 +62,8 @@ def generate_text(model, start_string):
         # Pass the predicted character as the next input to the model
         # along with the previous hidden state
         input_eval = tf.expand_dims([predicted_id], 0)
-
         text_generated.append(idx2char[predicted_id])
-
     return start_string + ''.join(text_generated)
 
 
-print(generate_text(model, start_string=u"@"))
+print(generate_text(model, u"@", 1000))
