8679c92f1b008f409b6ccdd1c4be98645d5f3c71
Qifan Zhang <firefighter-eric@users.noreply.github.com>
2020-11-30 03:08:42 -0500

    gitignore update

--- model.py
+++ model.py
@@ -1,18 +1,7 @@
 import tensorflow as tf
 
 
-def build_basic_model(vocab_size, rnn_units):
-    model = tf.keras.Sequential([
-        tf.keras.layers.GRU(rnn_units,
-                            return_sequences=True,
-                            stateful=True,
-                            recurrent_initializer='glorot_uniform'),
-        tf.keras.layers.Dense(vocab_size)
-    ])
-    return model
-
-
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+def build_basic_model(vocab_size, embedding_dim, rnn_units, batch_size):
     model = tf.keras.Sequential([
         tf.keras.layers.Embedding(vocab_size, embedding_dim,
                                   batch_input_shape=[batch_size, None]),
